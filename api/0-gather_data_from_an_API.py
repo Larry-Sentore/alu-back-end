9d4c64196c2670a8c09b2be05c48247ecd656df4
Larry-Sentore <l.sentore@alustudent.com>
2025-07-10 17:08:40 +0200

    yes

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -11,6 +11,7 @@ USER = "https://jsonplaceholder.typicode.com/users/" + str(userid)
 response = requests.get(URL)
 names = requests.get(USER)
 
+# Get the list of tasks and the name of the employee
 todo = response.json()
 datas = names.json()
 
@@ -20,6 +21,7 @@ for task in todo:
     if task['completed']:
         list.append(task['title'])
 
+# Print the name of the employee and the completed tasks
 print(f"Employee {name} is done with tasks {len(list)}/{len(todo)}")
 for item in list:
     print(item)

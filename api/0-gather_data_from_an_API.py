719bb3f07a4b37dcd0e7ff16945e7502fd7647d5
Larry-Sentore <l.sentore@alustudent.com>
2025-07-10 17:03:46 +0200

    yes

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
 
 import requests
+list = []
 
-id = int(input("Enter the ID of the employee: "))
+userid = int(input("Enter the ID of the employee: "))
 
-URL = "https://jsonplaceholder.typicode.com/todos/" + str(id)
+URL = "https://jsonplaceholder.typicode.com/todos/" 
+USER = "https://jsonplaceholder.typicode.com/users/" + str(userid)
 
 response = requests.get(URL)
+names = requests.get(USER)
 
-print(response.data)
+todo = response.json()
+datas = names.json()
+
+name = datas.get('name')
+
+for task in todo:
+    if task['completed']:
+        list.append(task['title'])
+
+print(f"Employee {name} is done with tasks {len(list)}/{len(todo)}")
+for item in list:
+    print(item)

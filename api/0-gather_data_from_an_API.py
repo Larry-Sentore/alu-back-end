8059af46f2364771ba1729792f40be36478ecdc5
Larry-Sentore <l.sentore@alustudent.com>
2025-07-14 19:40:43 +0200

    yes

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,44 +1,54 @@
 #!/usr/bin/python3
 """
-Fetch and display an employee's TODO progress from JSONPlaceholder.
-
-Usage: ./0-gather_data_from_an_API.py <EMPLOYEE_ID>
+    python script that returns TODO list progress for a given employee ID
 """
-
-import sys
+import json
 import requests
-
-BASE_URL = 'https://jsonplaceholder.typicode.com'
+from sys import argv
 
 
 if __name__ == "__main__":
-    """Entry point when run as a script."""
-    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
-        print("Usage: {} <EMPLOYEE_ID>".format(sys.argv[0]))
-        sys.exit(1)
+    """
+        request user info by employee ID
+    """
+    request_employee = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/'.format(argv[1]))
+    """
+        convert json to dictionary
+    """
+    employee = json.loads(request_employee.text)
+    """
+        extract employee name
+    """
+    employee_name = employee.get("name")
 
-    emp_id = int(sys.argv[1])
+    """
+        request user's TODO list
+    """
+    request_todos = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1]))
+    """
+        dictionary to store task status in boolean format
+    """
+    tasks = {}
+    """
+        convert json to list of dictionaries
+    """
+    employee_todos = json.loads(request_todos.text)
+    """
+        loop through dictionary & get completed tasks
+    """
+    for dictionary in employee_todos:
+        tasks.update({dictionary.get("title"): dictionary.get("completed")})
 
-    # 1. Get employee name
-    user_resp = requests.get("{}/users/{}".format(BASE_URL, emp_id))
-    if user_resp.status_code != 200:
-        print("Error: Employee with ID {} not found.".format(emp_id))
-        sys.exit(1)
-
-    employee_name = user_resp.json().get("name")
-
-    # 2. Get todos
-    todos_resp = requests.get("{}/todos".format(BASE_URL),
-                              params={"userId": emp_id})
-    todos = todos_resp.json()
-
-    # 3. Completed tasks
-    completed = [t for t in todos if t.get("completed")]
-
-    # 4. Print report
-    print("Employee {} is done with tasks({}/{}):"
-          .format(employee_name, len(completed), len(todos)))
-
-    for task in completed:
-        # one tab + one space exactly
-        print("\t {}".format(task.get("title")))
+    """
+        return name, total number of tasks & completed tasks
+    """
+    EMPLOYEE_NAME = employee_name
+    TOTAL_NUMBER_OF_TASKS = len(tasks)
+    NUMBER_OF_DONE_TASKS = len([k for k, v in tasks.items() if v is True])
+    print("Employee {} is done with tasks({}/{}):".format(
+        EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+    for k, v in tasks.items():
+        if v is True:
+            print("\t {}".format(k))
